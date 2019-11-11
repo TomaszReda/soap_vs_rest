15d12b8daaf497e9a5b1e3fce45801d4dd7c7808
tomek <GHJbnm123>
2019-11-11 22:01:58 +0100

    Start projektu

--- src/main/java/pl/tomasz_reda/praca_magisterska/soap_vs_rest/controller/AngularController.java
+++ src/main/java/pl/tomasz_reda/praca_magisterska/soap_vs_rest/controller/AngularController.java
@@ -1,12 +1,21 @@
 package pl.tomasz_reda.praca_magisterska.soap_vs_rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class AngularController {
 
     public static final String HOME_PAGE = "forward:/index.html";
 
     private String page() {
         return HOME_PAGE;
+    }
+
+    @GetMapping("/")
+    public String page(Model model) {
+        return page();
     }
 }
