d7b2bfb314119f17773275405e703eab02ef441d
tomek <GHJbnm123>
2019-11-11 21:47:43 +0100

    Initial commit

---
+++ src/main/java/pl/tomasz_reda/praca_magisterska/soap_vs_rest/SoapVsRestApplication.java
@@ -0,0 +1,13 @@
+package pl.tomasz_reda.praca_magisterska.soap_vs_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapVsRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapVsRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tomasz_reda/praca_magisterska/soap_vs_rest/SoapVsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomasz_reda.praca_magisterska.soap_vs_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapVsRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
