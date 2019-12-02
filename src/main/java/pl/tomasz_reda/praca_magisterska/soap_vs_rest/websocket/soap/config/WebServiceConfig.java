5e03b9c6f35af60360b7eaea5c74182ab110c361
treda <tomekreda16@gmail.com>
2019-12-02 22:08:28 +0100

    Dodanie soapa

---
+++ src/main/java/pl/tomasz_reda/praca_magisterska/soap_vs_rest/websocket/soap/config/WebServiceConfig.java
@@ -0,0 +1,57 @@
+package pl.tomasz_reda.praca_magisterska.soap_vs_rest.websocket.soap.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "users")
+    public DefaultWsdl11Definition userWsdl11Definition(XsdSchema userSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("UserPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("https://www.roytuts.com/UserService");
+        wsdl11Definition.setSchema(userSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema userSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/user.xsd"));
+    }
+
+    @Bean(name = "books")
+    public DefaultWsdl11Definition bookWsdl11Definition(XsdSchema bookSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("UserPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("https://www.roytuts.com/UserService");
+        wsdl11Definition.setSchema(bookSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema bookSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/book.xsd"));
+    }
+}
