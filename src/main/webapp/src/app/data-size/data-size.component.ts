8192180b4b0d871a56d3800e38daa6fe4ed3710f
treda <tomekreda16@gmail.com>
2019-12-22 13:28:35 +0100

    Rozmiar danych

--- src/main/webapp/src/app/data-size/data-size.component.ts
+++ src/main/webapp/src/app/data-size/data-size.component.ts
@@ -7,6 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataSizeComponent implements OnInit {
 
+  soap='curl -X POST http://localhost:8080/ws -H \'Content-Type: text/xml\' -H \'SOAPAction: blz:getBank\' -d \'\n' +
+    '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sch="https://www.praca_magister/soap_vs_rest/books.com">\n' +
+    '<soapenv:Header/>\n' +
+    '<soapenv:Body>\n' +
+    '<sch:AddBookRequest>\n' +
+    '<sch:author>Autor</sch:author>\n' +
+    '<sch:title>Tytul</sch:title>\n' +
+    '<sch:publisher>Publikacja</sch:publisher>\n' +
+    '<sch:quant>4</sch:quant>\n' +
+    '<sch:bookSearch>BOok search</sch:bookSearch>\n' +
+    '<sch:description>Description</sch:description>\n' +
+    '<sch:isbn>isbn</sch:isbn>\n' +
+    '<sch:data>2002-05-30T09:00:00</sch:data>\n' +
+    '<sch:bookState>NOTRESERVED</sch:bookState>\n' +
+    '<sch:bookCategory>\n' +
+    '<sch:id>1</sch:id>\n' +
+    '<sch:categoryType>Fantasy</sch:categoryType>\n' +
+    '</sch:bookCategory>\n' +
+    '</sch:AddBookRequest>\n' +
+    '</soapenv:Body>\n' +
+    '</soapenv:Envelope>\'';
+
+  rest='curl -d \'\n' +
+    '{\n' +
+    '"id": "",\n' +
+    '"author": "author4",\n' +
+    '"title": "title4",\n' +
+    '"publisher": "publisher4",\n' +
+    '"date": "2019-12-01","isbn": "isbn4",\n' +
+    '"quant": 4,\n' +
+    '"bookSearch": "bookSearch4",\n' +
+    '"description": "description4",\n' +
+    '"bookState": "NOTRESERVED",\n' +
+    '"bookCategory": [\n' +
+    '{\n' +
+    '"id": 1,\n' +
+    '"categoryType": "Fantasy"\n' +
+    '}\n' +
+    ']\n' +
+    '}\n' +
+    '\' -H \'Content-Type: application/json\' localhost:8080/api/rest/book/add';
   constructor() { }
 
   ngOnInit() {
