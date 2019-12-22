ec4182352bf024c0ea50e87d99789b7bb96941ee
treda <tomekreda16@gmail.com>
2019-12-22 16:27:56 +0100

    Komunikacja  z serwerem

--- src/main/webapp/src/app/serwer/serwer.component.ts
+++ src/main/webapp/src/app/serwer/serwer.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SerwerComponent implements OnInit {
 
+  soap='curl -X POST http://localhost:8080/ws -H \'Content-Type: text/xml\' -H \'SOAPAction: blz:getBank\' -d \'\n' +
+    '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sch="https://www.praca_magister/soap_vs_rest/users.com">\n' +
+    '<soapenv:Header/>\n' +
+    '<soapenv:Body>\n' +
+    '<sch:DeleteUserRequest>\n' +
+    '<sch:id>9</sch:id>\n' +
+    '</sch:DeleteUserRequest>\n' +
+    '</soapenv:Body>\n' +
+    '</soapenv:Envelope>\'';
+  rest='curl -X DELETE localhost:8080/api/rest/user/delete/5'
   constructor() { }
 
   ngOnInit() {
