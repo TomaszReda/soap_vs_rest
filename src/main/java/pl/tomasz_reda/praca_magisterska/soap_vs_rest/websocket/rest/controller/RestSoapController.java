78a9d590e424625c651c8d850e619ebf754cd546
treda <tomekreda16@gmail.com>
2019-12-22 18:44:30 +0100

    Wykorzystanie rest-soap

--- src/main/java/pl/tomasz_reda/praca_magisterska/soap_vs_rest/websocket/rest/controller/RestSoapController.java
+++ src/main/java/pl/tomasz_reda/praca_magisterska/soap_vs_rest/websocket/rest/controller/RestSoapController.java
@@ -1,17 +1,29 @@
 package pl.tomasz_reda.praca_magisterska.soap_vs_rest.websocket.rest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.tomasz_reda.praca_magisterska.soap_vs_rest.model.user.User;
+import pl.tomasz_reda.praca_magisterska.soap_vs_rest.repository.UserRepository;
+import soap.DeleteUserRequest;
+
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/")
 public class RestSoapController {
 
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("rest_to_soap")
-    public void a() {
-
-
+    @PostMapping("rest_to_soap/delete/user/{id}")
+    public void a(@RequestBody DeleteUserRequest deleteUserRequest) throws Exception {
+        Optional<User> user = userRepository.findById(deleteUserRequest.getId());
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+        }
     }
 }
--- src/main/webapp/src/app/utilization/utilization.component.ts
+++ src/main/webapp/src/app/utilization/utilization.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UtilizationComponent implements OnInit {
 
+  rest='@RestController\n' +
+    '@RequestMapping("/api/")\n' +
+    'public class RestSoapController {\n' +
+    '\n' +
+    '    @Autowired\n' +
+    '    private UserRepository userRepository;\n' +
+    '\n' +
+    '    @PostMapping("rest_to_soap/delete/user/{id}")\n' +
+    '    public void a(@RequestBody DeleteUserRequest deleteUserRequest) throws Exception {\n' +
+    '        Optional<User> user = userRepository.findById(deleteUserRequest.getId());\n' +
+    '        if (user.isPresent()) {\n' +
+    '            userRepository.delete(user.get());\n' +
+    '        }\n' +
+    '    }\n' +
+    '}\n';
   constructor() { }
 
   ngOnInit() {
